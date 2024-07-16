b9a0fa7a873fc7bf1286f99cfcd451ad7a462c1f
Joee <gachaujoe@gmail.com>
2024-07-16 20:59:25 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Your code goes here
+// index.js
+document.addEventListener('DOMContentLoaded', function() {
+    const paragraph = document.querySelector('p');
+    paragraph.textContent = "This is really cool!";
+  });
+//   require('./helpers.js');
+const { expect } = require('chai');
+
+describe("index.js", () => {
+  it('should change the text on the page when DOM content has loaded', function() {
+    // Initial text before DOMContentLoaded
+    expect(document.querySelector('p').textContent).to.equal("JavaScript is so cool. It lets me add text to my page programmatically.");
+
+    // Simulate the DOMContentLoaded event
+    const event = document.createEvent('Event');
+    event.initEvent('DOMContentLoaded', true, true);
+    window.document.dispatchEvent(event);
+
+    // Text after DOMContentLoaded
+    expect(document.querySelector('p').textContent).to.equal("This is really cool!");
+  });
+});
